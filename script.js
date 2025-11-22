46da78b6a8115d170fe648aae3a65f7b35aabf8d
nguyenphamchithanh2005-pixel <nguyenphamchithanh2005@gmail.com>
2025-11-22 13:37:57 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Portfolio của Chí Thành đã load xong!');
-
-    alert('Chào mừng bạn đến với portfolio trên Internet của mình!');
-});
